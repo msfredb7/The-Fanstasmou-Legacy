adc6283244b8e12645601a80cc64eb1130493bd4
Xeal <alexandre-blanchet94@hotmail.com>
2018-03-02 18:34:32 -0500

    Axis Controls and fix

---
+++ UnityProject - GJH18/Assets/Game/Game/Controls/InputPlayerAxis.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayerAxis : MonoBehaviour {
+
+    public enum PlayerNumber
+    {
+        One = 0,
+        Two = 1,
+        Three = 2,
+        Four = 3
+    }
+
+    public PlayerNumber player;
+	
+	public float GetPlayerAxis(string direction)
+    {
+        float result = 0;
+        switch (player)
+        {
+            default:
+            case PlayerNumber.One:
+                result = Input.GetAxis(direction + "PlayerOne");
+                break;
+            case PlayerNumber.Two:
+                result = Input.GetAxis(direction + "PlayerTwo");
+                break;
+            case PlayerNumber.Three:
+                result = Input.GetAxis(direction + "PlayerThree");
+                break;
+            case PlayerNumber.Four:
+                result = Input.GetAxis(direction + "PlayerFour");
+                break;
+        }
+        return result;
+    }
+
+    public float GetPlayerHorizontal()
+    {
+        return GetPlayerAxis("Horizontal");
+    }
+
+    public float GetPlayerVertical()
+    {
+        return GetPlayerAxis("Vertical");
+    }
+}
