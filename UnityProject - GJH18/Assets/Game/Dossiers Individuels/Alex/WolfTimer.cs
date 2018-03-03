b5f3de0c3839f6160789b4489489f90266697bf2
Xeal <alexandre-blanchet94@hotmail.com>
2018-03-03 16:43:30 -0500

    UI Countdown pour l'intro wolf terminado amigo

---
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/Alex/WolfTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WolfTimer : MonoBehaviour {
+
+    public Text counter;
+
+    private int currentValue;
+    private float currentSecondAmount = 0;
+
+	public void SetCounterValue(int value)
+    {
+        currentValue = value;
+        counter.text = "" + currentValue;
+        currentSecondAmount = 0;
+    }
+
+    void Update()
+    {
+        currentSecondAmount += Time.deltaTime;
+        if(currentSecondAmount > 1)
+            SetCounterValue(--currentValue);
+    }
+}
---
+++ UnityProject - GJH18/Assets/Plugins/CCC/Animation/FlashColor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FlashColor : MonoBehaviour {
+
+    public Color newColor;
+    public float animDuration = 1;
+
+    public enum ComponentType { noFade = 0, Text = 1, Image = 2 }
+    public ComponentType currentType = ComponentType.noFade;
+
+    public bool onStart = true;
+
+    void Start()
+    {
+        if (onStart)
+            Flash();
+    }
+
+    void Flash()
+    {
+        switch (currentType)
+        {
+
+            default:
+            case ComponentType.noFade:
+                break;
+            case ComponentType.Text:
+                Sequence sqc1 = DOTween.Sequence();
+                sqc1.Append(GetComponent<Text>().DOColor(newColor, animDuration));
+                sqc1.SetLoops(-1, LoopType.Yoyo);
+                break;
+            case ComponentType.Image:
+                Sequence sqc2 = DOTween.Sequence();
+                sqc2.Append(GetComponent<Image>().DOColor(newColor, animDuration));
+                sqc2.SetLoops(-1, LoopType.Yoyo);
+                break;
+        }
+    }
+}
