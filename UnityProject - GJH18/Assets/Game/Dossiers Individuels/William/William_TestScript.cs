7aabe57996929d2f83082c484b17ee86f3934da8
Willbur415 <dery.william@hotmail.com>
2018-03-03 10:27:18 -0500

    Fixed My scene

--- UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/William_TestScript.cs
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/William_TestScript.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class William_TestScript : MonoBehaviour
 {
-  
+    public SceneInfo GameScene;
+
+    private void Start()
+    {
+        LoadingScreen.TransitionTo(GameScene.SceneName, null);
+    }
 }
