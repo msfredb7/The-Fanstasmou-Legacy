08845c7674e617c797c8c8636023f3457aae5172
Willbur415 <dery.william@hotmail.com>
2018-03-02 20:43:12 -0500

    Team selection done *Only need to switch to game scene to be complet*

---
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/InputPlayerButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayerButton : MonoBehaviour {
+
+    public InputPlayerAxis.PlayerNumber player;
+
+	public bool GetPlayerStart()
+    {
+        return Input.GetButton("StartPlayer" + player);
+    }
+}
---
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/SelectionInputs.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionInputs : MonoBehaviour {
+    public enum Team
+    {
+        None,
+        Shepherd,
+        Wolf
+    }
+    InputPlayerAxis axis;
+    InputPlayerButton buttons;
+    RectTransform rect;
+
+    public Team team;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        axis = GetComponent<InputPlayerAxis>();
+        buttons = GetComponent<InputPlayerButton>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (axis.GetPlayerHorizontal() > 0)
+            changeTeam("wolfs");
+        else if (axis.GetPlayerHorizontal() < 0)
+        {
+            changeTeam("shepherds");
+        }
+        
+        if (buttons != null)
+        {
+            if (buttons.GetPlayerStart())
+            {
+                //PlayerPrefs.SetString("player" + axis.player + "team", )
+            }
+        }
+    }
+
+    private void changeTeam(string _teamName)
+    {
+        if (_teamName == "wolfs")
+        {
+            team = Team.Wolf;
+            rect.anchoredPosition = new Vector3(250, rect.anchoredPosition.y);
+        }
+        else if (_teamName == "shepherds")
+        {
+            team = Team.Shepherd;
+            rect.anchoredPosition = new Vector3(-250, rect.anchoredPosition.y);
+        }
+    }
+}
