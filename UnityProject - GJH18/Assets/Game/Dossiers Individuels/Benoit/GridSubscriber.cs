d34864be3ed7b54222f7d2f929868a68a30f9a49
Benoit Bergeron <Fivocx@gmail.com>
2018-03-03 04:43:37 -0500

    Delay dans grid

--- UnityProject - GJH18/Assets/Game/Dossiers Individuels/Benoit/GridSubscriber.cs
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/Benoit/GridSubscriber.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+public struct NeighborsRequest
+{
+    public float lifetime;
+    public List<GameObject> neighbors;
+    public float range;
+    public System.Type type;
+    public bool delayed;
+
+    public NeighborsRequest(System.Type tp, float life, float rg){
+        type = tp;
+        lifetime = life;
+        range = rg;
+        delayed = false;
+        neighbors = new List<GameObject>();
+    }
+}
+
+
+
+
 public class GridSubscriber: MonoBehaviour
 {
-    [HideInInspector]
-    public Vector2Int currentCell = new Vector2Int(-1 ,-1);
+    private static int instanceCount = 0;
+    private int instanceNumber;
 
-    Vector2 position { get  { return (Vector2)gameObject.transform.position; } }
+    [HideInInspector]
+    public Vector2Int currentCell = new Vector2Int(-1, -1);
+
+    public int RequestPerSeconds = 5;
+
+    private Vector2 position { get  { return (Vector2)gameObject.transform.position; } }
+    private List<NeighborsRequest> neighborsRequest = new List<NeighborsRequest>();
+
+    
 
     public void Start()
     {
+        instanceNumber = instanceCount++;
         Update();
     }
 
@@ -19,20 +50,66 @@ public class GridSubscriber: MonoBehaviour
         Vector2Int newCell = UnitGrid.Instance.WorldToGrid(position);
         if (newCell != currentCell)
             UnitGrid.Instance.SuscribeUnit(this, newCell);
+
+
+        for (int i = 0; i < neighborsRequest.Count; i++)
+        {
+            var nRequest = (neighborsRequest[i]);
+            nRequest.lifetime -= Time.deltaTime;
+            neighborsRequest[i] = nRequest;
+        }
     }
+
 
     public List<GameObject> GetNeighbors<T>(float range) where T : UnityEngine.MonoBehaviour
     {
-        List<GameObject> objects = UnitGrid.Instance.GetObjectsInRange<T>(gameObject.transform.position, range);
-        for (int i = 0; i < objects.Count; i++)
+        for (int i = 0; i < neighborsRequest.Count; i++)
         {
-            if (objects[i] == gameObject)
-                objects.RemoveAt(i);
+            if (neighborsRequest[i].type == typeof(T) && neighborsRequest[i].range == range)
+            {
+                if (neighborsRequest[i].lifetime > 0)
+                    return neighborsRequest[i].neighbors;
+                else
+                    return ResetNRequest<MonoBehaviour>(i, range);   
+            }
         }
+        return NewNRequest<T>(range);
+    }
+       
+    private List<GameObject> NewNRequest<T>(float range) where T : UnityEngine.MonoBehaviour
+    {
+        NeighborsRequest newNRequest = new NeighborsRequest(typeof(T), 1 / RequestPerSeconds, range);
+        neighborsRequest.Add(newNRequest);
+        return ResetNRequest<T>(neighborsRequest.IndexOf(newNRequest), range);
+    }
+    
+    private List<GameObject> ResetNRequest<T>(int i, float range) where T : UnityEngine.MonoBehaviour
+    {
+        var nRequest = neighborsRequest[i];
+        nRequest.neighbors = RefreshNeighbors<T>(range);
+        nRequest.lifetime = 1 / RequestPerSeconds;
+        if (!nRequest.delayed)
+        {
+            nRequest.lifetime *= (1 - ((instanceNumber % 10) / 10));
+            nRequest.delayed = true;
+        }
+        neighborsRequest[i] = nRequest;
+        return nRequest.neighbors;
+    }
 
-        Debug.Log(objects.Count);
-
-        return objects;
+    private List<GameObject> RefreshNeighbors<T>(float range) where T : UnityEngine.MonoBehaviour
+    {
+        List<GameObject> neighbors = UnitGrid.Instance.GetObjectsInRange<T>(gameObject.transform.position, range);
+        for (int i = 0; i < neighbors.Count; i++)
+        {
+            if (neighbors[i] == gameObject)
+                neighbors.RemoveAt(i);
+        }
+        return neighbors;
     }
 }
+
+
+
+
 
---
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/Benoit/Herd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Herd : MonoBehaviour {
+
+    [SerializeField]
+    private List<SheepComponent> herdMembers = new List<SheepComponent>();
+
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
