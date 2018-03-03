a96e3682691c05c264b5515df97c0b86ef2d8da2
Gabriel-PC <gab_sim16@hotmail.com>
2018-03-03 15:48:45 -0500

    Ajout de la zone de spawn de sheep, à placer sur la map. S = Spawn dans la zone et C = Spawn sur la sourie !!!!!!!!!!!!!!

---
+++ UnityProject - GJH18/Assets/SpawnerSheep.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerSheep : MonoBehaviour {
+
+    public GameObject m_BordHautDroit;
+    public GameObject m_BordBasGauche;
+
+    public GameObject m_SheepPrefab;
+    public Transform m_UnitContain;
+
+    public int m_NbSheepStart;
+
+    float m_xMin, m_xMax, m_yMin, m_yMax;
+
+	// Use this for initialization
+	void Start () {
+        m_xMin = m_BordBasGauche.transform.position.x;
+        m_xMax = m_BordHautDroit.transform.position.x;
+        m_yMin = m_BordBasGauche.transform.position.y;
+        m_yMax = m_BordHautDroit.transform.position.y;
+
+        SpawnPref(m_NbSheepStart);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("s"))
+            SpawnPref(1);
+
+        if (Input.GetKeyDown("c"))
+            SpawnPrefAtLocation((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition));
+	}
+
+    private void SpawnPref(int NbToSpawn)
+    {
+        Vector3 pos;
+
+        for (int i = 0; i < NbToSpawn; i++)
+        {
+            pos = new Vector3(Random.Range(m_xMin, m_xMax), Random.Range(m_yMin, m_yMax));
+            Instantiate(m_SheepPrefab, pos, Quaternion.identity, m_UnitContain);
+        }
+    }
+
+    private void SpawnPrefAtLocation(Vector2 loc)
+    {
+        Instantiate(m_SheepPrefab, loc, Quaternion.identity, m_UnitContain);
+    }
+}
