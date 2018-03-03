83541d24563a9a31de24624afb18facae2da67e7
Willbur415 <dery.william@hotmail.com>
2018-03-03 12:38:54 -0500

    Menu sound and refinement

--- UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/SelectionInputs.cs
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/SelectionInputs.cs
@@ -11,8 +11,17 @@ public class SelectionInputs : MonoBehaviour {
     }
     InputPlayerAxis axis;
     RectTransform playerRect;
+
     [SerializeField]
     private RectTransform menuTransform;
+
+    [SerializeField]
+    private GameObject menuAudioObject;
+
+    private MenuAudio menuAudio;
+
+    private bool teamSelected = false;
+    private bool soundOnCooldown = false;
 
     public Team team;
 
@@ -20,30 +29,55 @@ public class SelectionInputs : MonoBehaviour {
     {
         playerRect = GetComponent<RectTransform>();
         axis = GetComponent<InputPlayerAxis>();
+        menuAudio = menuAudioObject.GetComponent<MenuAudio>();
+
     }
 
     private void FixedUpdate()
     {
         if (axis.GetPlayerHorizontal() > 0.50f)
+        {
+            if (team != Team.Wolf)
             changeTeam(1);
+        }
         else if (axis.GetPlayerHorizontal() < -0.50f)
         {
-            changeTeam(0);
+            if (team != Team.Shepherd)
+                changeTeam(0);
         }
     }
     
     private void changeTeam(int _teamName)
     {
-        float playerSpritePosition = menuTransform.rect.width/4;
-        if (_teamName == 1)
+        if (!teamSelected)
         {
-            team = Team.Wolf;
-            playerRect.anchoredPosition = new Vector3(playerSpritePosition + playerRect.rect.width, playerRect.anchoredPosition.y);
-        }
-        else if (_teamName == 0)
-        {
-            team = Team.Shepherd;
-            playerRect.anchoredPosition = new Vector3(-playerSpritePosition, playerRect.anchoredPosition.y);
+            if (!soundOnCooldown)
+            {
+                menuAudio.PlayNavigationSound();
+                soundOnCooldown = true;
+                this.DelayedCall(() => 
+                {
+                    soundOnCooldown = false;
+                }, 0.3f);
+            }
+
+            float playerSpritePosition = menuTransform.rect.width / 4;
+            if (_teamName == 1)
+            {
+                team = Team.Wolf;
+                playerRect.anchoredPosition = new Vector3(playerSpritePosition + playerRect.rect.width, playerRect.anchoredPosition.y);
+            }
+            else if (_teamName == 0)
+            {
+                team = Team.Shepherd;
+                playerRect.anchoredPosition = new Vector3(-playerSpritePosition, playerRect.anchoredPosition.y);
+            }
+            teamSelected = true;
+
+            this.DelayedCall(() =>
+            {
+                teamSelected = false;
+            }, 0.3f);
         }
     }
 }
