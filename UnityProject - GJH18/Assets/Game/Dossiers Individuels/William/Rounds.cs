377a8280f1623b7c056b2993ceb361e190acd525
Willbur415 <dery.william@hotmail.com>
2018-03-04 02:35:11 -0500

    Added rounds and removed a misplaced wolf prefab

---
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/Rounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rounds : MonoBehaviour {
+
+    public int CurrentRounnd;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void BeginRound()
+    {
+
+    }
+
+    public void EndRound()
+    {
+
+    }
+}
