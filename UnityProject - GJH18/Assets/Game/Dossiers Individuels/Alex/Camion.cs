c0724184e4f06807a7276397c68b0b0a0e2f3c23
Benoit Bergeron <Fivocx@gmail.com>
2018-03-04 10:31:54 -0500

    merge

--- UnityProject - GJH18/Assets/Game/Dossiers Individuels/Alex/Camion.cs
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/Alex/Camion.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Camion : MonoBehaviour {
 
     public int amountOfSheep = 1;
 
+
+    //public BoxCollider2D collsiion;
+    public GameObject plateformCheckpoint;
+    public GameObject camionCheckpoint;
+    public float EvacSpeed = 1.25f;
+
+
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.GetComponent<SheepComponent>() != null)
+        HerdMember sheep = collider.GetComponent<HerdMember>();
+        if (sheep != null)
         {
-            Debug.Log("TODO: +1 Sheep saved !");
-            Debug.Log("TODO: delete sheep");
+            EvacSheep(sheep);
             Game.Instance.carManager.ExitCar(gameObject);
         }
+    }
+
+    public void EvacSheep(HerdMember sheep)
+    {
+        Sequence sqc = DOTween.Sequence();
+        MoveToCheckpoint(sheep.transform, (Vector2)plateformCheckpoint.transform.position, sqc);
+        MoveToCheckpoint(sheep.transform, (Vector2)camionCheckpoint.transform.position, sqc);
+        sheep.Evac();
+    }
+
+    public void MoveToCheckpoint(Transform sheep, Vector2 checkpoint, Sequence sqc)
+    {
+        float rotationAngle = ((Vector2)sheep.transform.position - checkpoint).ToAngle();
+        Vector3 rotation = transform.forward * rotationAngle;
+        sheep.rotation = Quaternion.Euler(rotation);
+
+        float distance = (checkpoint - (Vector2)sheep.position).magnitude;
+        float animationLength = distance / EvacSpeed;
+
+        sqc.Append(sheep.DOMove((Vector3)checkpoint, animationLength));
     }
 }
