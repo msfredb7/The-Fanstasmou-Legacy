ef4096cc81cb6fb36ba062e6bf7b93891bd4b603
Willbur415 <dery.william@hotmail.com>
2018-03-04 15:04:01 -0500

    End game stats and UI indicator

---
+++ UnityProject - GJH18/Assets/Game/Dossiers Individuels/William/ShowUIStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowUIStats : MonoBehaviour {
+
+    public bool isLeftTeam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isLeftTeam)
+        {
+            GetComponent<Text>().text = Rounds.Instance.TeamOne.NBSheepRescued.ToString();
+        }
+        else
+        {
+            GetComponent<Text>().text = Rounds.Instance.TeamTwo.NBSheepRescued.ToString();
+        }
+	}
+}
